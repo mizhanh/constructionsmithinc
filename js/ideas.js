6f060b8f5adcc32563bccd92fc550573a9574c2b
mizhanh <31321795+mizhanh@users.noreply.github.com>
2018-08-17 13:20:13 -0400

    video function completed and worked

--- js/ideas.js
+++ js/ideas.js
@@ -1,42 +1,52 @@
-$(document).ready(function () {
-    $('#search-btn').submit(function (event) {
-        event.preventDefault();
-        var q = $('#query').val().trim();
-        search(q);
+var gapikey = 'AIzaSyBoQiw1Pr-LoMT3OPR-PcqZ44NO3kXF3TY';
+
+$(function() {
+    $('#search-form').submit( function(e) {
+        e.preventDefault();
+        $('#seeded-videos').hide();
     });
-
-
-function search(q) {
-    URL = 'https://www.googleapis.com/youtube/v3/search';
-    var params = {
-        part: 'snippet, id',
-        q: q,
-        type: video,
-        key: 'AIzaSyBoQiw1Pr-LoMT3OPR-PcqZ44NO3kXF3TY'};
-
-    $.get(URL, params, function (data) {
-        $.each(data.items, function(i, item) {
-            console.log(data);
-            var output = getOutput(item);
-            $('#search-results').append(output);
-        });
-    });
-}
-
-function getOutput(item){
-    var videoId = item.id.videoId;
-    var title = item.snippet.title;
-    var thumbnails = item.snippet.thumbnails.high.url;
-
-    var output = '<li>' +
-        '<div class="list-left">' +
-        '<img src="'+thumbnails+'">' +
-        '</div>' +
-        '<div class="list-right">' + 
-        '<h3>'+ title +'</h3>'; 
-}
-
 });
+
+function searchYoutube() {
+    $('#results').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Loading...</span>');
+    $('#buttons').html('');
+    
+    // get form input
+    q = $('#search').val(); 
+    
+    // run get request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            type: 'video',
+            maxResults: 4,
+            key: gapikey
+        }, function(data) {
+            console.log(data);
+            $('#results').html(''); // hide loading
+
+            for (var i=0; i<data.items.length; i++) {
+            console.log(data.items.length);
+            var v = data.items[i];
+            console.log(v);
+            var output = getOutput(v);
+            $('#results').append(output);
+            }
+            
+    });
+}
+
+// Build output
+function getOutput(item) {
+    var videoID = item.id.videoId;
+    var title = item.snippet.title;
+    
+    var output = '<iframe width="450" height="375" src="https://www.youtube.com/embed/' + videoID + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen position position="relative"></iframe>';
+
+    return output;
+}
+
 
 
 
